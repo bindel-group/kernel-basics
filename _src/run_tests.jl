c12501859d55840dac29ff5ae850f742282bf055
David Bindel <bindel@cs.cornell.edu>
2025-07-23 13:10:05 -0400

    Move ldoc build output to _ldoc subdir.

---
+++ _src/run_tests.jl
@@ -0,0 +1,17 @@
+using LinearAlgebra
+using StatsFuns
+using SpecialFunctions
+using Optim
+using Test
+
+include("testing.jl")
+include("sample.jl")
+include("kfuns.jl")
+include("kmats.jl")
+include("gpp.jl")
+include("hypers.jl")
+include("acquisition.jl")
+include("bo_step.jl")
+
+include("test_kfuns.jl")
+include("test_kmats.jl")
